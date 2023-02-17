dffa03fb8e81f29c7e522cfba93184a9f053b140
root <hinestrozasantiago@gmail.com>
2023-02-17 08:08:11 -0800

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -a C "ith proper grammar,
+ * main -a C "ith proper grammar,but the outcome is a piece of art,
  *
  * Return: 0 (Sucess);
  */
 int main(void)
 {
-puts("\"but the outcome is a piece of artbut the outcome is a piece of art,\n");
+puts("but the outcome is a piece of artbut the outcome is a piece of art,\n");
 return (0);
 }
