142ffe0d80a3101122e260e9535c26e516c008c0
root <hinestrozasantiago@gmail.com>
2023-02-17 07:56:40 -0800

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -a C "Programming is like building
+ *
+ * Return: 0 (Sucess);
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
