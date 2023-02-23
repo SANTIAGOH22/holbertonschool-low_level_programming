7444abebd53c0c7d55bd1896d45290e8b644cd05
root <hinestrozasantiago@gmail.com>
2023-02-22 20:21:59 -0800

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ * description - putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
