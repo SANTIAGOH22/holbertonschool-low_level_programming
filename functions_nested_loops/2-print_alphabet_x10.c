eb9b25a7cfb64c36d74734645506dd7fb0e1a188
root <hinestrozasantiago@gmail.com>
2023-02-23 18:58:12 -0800

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - function
+ * description - print alphabet ten times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char x, u;
+for (u = 0; u <= 9; u++)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+}
+}
