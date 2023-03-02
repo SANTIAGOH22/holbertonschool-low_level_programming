9ac453235f241047191ccffe9774ae3cc9b05157
root <hinestrozasantiago@gmail.com>
2023-03-02 11:24:31 -0800

    strncat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates string from other string
+ * @dest: 
+ * @src: 
+ *
+ * Return: string concatenated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0, a;
+while (dest[i] != '\0')
+i++;
+for (a = 0; src[a] != '\0'; a++, i++)
+dest[i] = src[a];
+dest[i] = '\0';
+return (dest);
+}
