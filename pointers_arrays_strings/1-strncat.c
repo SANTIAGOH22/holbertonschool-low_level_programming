f9a8c0b1a92a394dcc1a041b08cf9e594cc30745
root <hinestrozasantiago@gmail.com>
2023-03-03 08:55:45 -0800

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,10 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, n;
-while (dest[i] != '\0')
-i++;
-for (n = 0; n < n && src[n] != '\0'; n++, i++)
-dest[i] = src[n];
-dest[i] = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
