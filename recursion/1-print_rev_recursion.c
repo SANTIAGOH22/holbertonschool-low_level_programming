13bdab8b7073566c482c7aac0cc847cb9fb1d3d0
root <hinestrozasantiago@gmail.com>
2023-03-06 12:11:11 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * description - prints a string
+ * @s: argument for the function
+ *
+ */
+
+void _puts_recursion(char *s);
+{
+printf("%s\n", s);
